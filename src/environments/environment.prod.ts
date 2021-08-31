6d3c0e3bd0e11f87873074bf128e80d21a263db7
Josh Smith <joshuap.smith12@gmail.com>
2021-08-30 20:34:19 -0500

    Push environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCcSTnhKLdbqWZoyx8kN9JD9742jFbQs9k",
+    authDomain: "football-fantasy-8afc4.firebaseapp.com",
+    projectId: "football-fantasy-8afc4",
+    storageBucket: "football-fantasy-8afc4.appspot.com",
+    messagingSenderId: "964018369090",
+    appId: "1:964018369090:web:52b4e83f83a0e4eb25f3db"
+  }
 };
